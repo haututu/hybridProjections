074d6b2d34a7b0c26bb2cfb5a781c9595f927142
Haututu <35088807+haututu@users.noreply.github.com>
2018-07-02 20:56:37 +1200

    Replaced birth projections with binomial model
    Now using binomial model for births, using the shorter population time series means there is less data being modelled. Had to increase simulations a heap though.

--- projectBirthRates.R
+++ projectBirthRates.R
@@ -1,36 +1,60 @@
 library(tidyverse)
 library(demest)
 
-#Setup
-births <- read_csv("raw/births.csv") %>%
+
+
+###########Setup
+#Birth rates
+birthRates <- read_csv("raw/births.csv") %>%
   gather(age, rate, 2:41) %>%
   mutate(rate = rate/1000) %>%
-  filter(year >= 1996) %>%
-  xtabs(rate ~ age + year, data=.) %>%
-  Values(dimscales = c(year = "Intervals"))
+  filter(year >= 2013) %>%
+  mutate(age = sapply(str_split(age, " "), `[`, 1),
+         age = ifelse(age == 50, "50+", age))
+
+#Population counts
+pop <- read_csv("raw/pop2013-2017.csv") %>%
+  gather(age, pop, 3:93) %>%
+  filter(sex == "female") %>%
+  select(-sex) %>%
+  mutate(age = sapply(str_split(age, " "), `[`, 1),
+         age = ifelse(age >= 50, "50+", age)) %>%
+  group_by(year, age) %>%
+  summarise(pop = sum(pop))
+
+#Merge exposed population with birth rates
+births <- inner_join(birthRates, pop, by = c("year", "age")) %>%
+  mutate(births = as.integer(pop*rate))
+
+#Demographic arrays for modelling
+y <- xtabs(births ~ age + year, data=births) %>%
+  Counts(dimscales = c(year = "Intervals"))
+
+exposed <- xtabs(pop ~ age + year, data=births) %>%
+  Counts(dimscales = c(year = "Intervals"))
 
 #Model
-birth.model <- Model(y ~ Normal(mean ~ age * year))
+birth.model <- Model(y ~ Binomial(mean ~ age + year),
+                     age ~ DLM(error = Error(robust = TRUE)),
+                     jump = 0.045)
 
-ests <- "out/birthNormal.est"
+ests <- "out/birthBinomial.est"
   
 estimateModel(model = birth.model,
-              y = births,
+              y = y,
+              exposure = exposed,
               filename = ests,
-              nBurnin = 2000,
-              nSim = 10000,
+              nBurnin = 5000,
+              nSim = 60000,
               nChain = 4,
-              nThin = 4)
+              nThin = 30)
 
 fetchSummary(ests)
 
 #Forecast
-preds <- "out/birthNormal.pred"
+preds <- "out/birthBinomial.pred"
 
 predictModel(filenameEst = ests,
              filenamePred = preds,
              n = 11)
 
-rates.both <- fetchBoth(filenameEst = ests,
-                         filenamePred = preds,
-                         where = c("model", "likelihood", "mean"))
